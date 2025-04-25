ed9196bf9080acc2f3ad1d9132fa86da014cae03
Andriy Ivanchenko <mais-nasinya@ukr.net>
2025-04-25 12:14:40 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Не забудь імпортувати BrowserRouter
+import App from "./App.jsx";
+import "./index.css";
+import React from "react";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement); // Створення кореня
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
